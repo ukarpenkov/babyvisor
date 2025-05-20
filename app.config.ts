1068ce28d501b1cdd41b33af8b6724662c2b02bb
ukarpenkov <truloveu@gmail.com>
2025-05-20 21:46:50 +0300

    feat: initialize project with package.json, reset script, and TypeScript configuration

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+import { ExpoConfig } from "expo/config";
+import "ts-node/register"; // Add this to import TypeScript files
+
+const config: ExpoConfig = {
+  name: "my-app",
+  slug: "my-app",
+};
+
+export default config;
