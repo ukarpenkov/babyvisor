61fad8a6b373305d0af705cc30507d20e5a647a2
ukarpenkov <truloveu@gmail.com>
2025-05-27 11:16:42 +0300

    apk build

--- app.config.ts
+++ app.config.ts
@@ -4,8 +4,14 @@ import 'ts-node/register' // Add this to import TypeScript files
 const config: ExpoConfig = {
     name: 'BabyVisor',
     slug: 'BabyVisor',
+    owner: 'yurijs',
     android: {
         package: 'com.yurijs.BabyVisor',
+    },
+    extra: {
+        eas: {
+            projectId: '77fdde13-76f7-4077-a49e-c4b153eca4f4',
+        },
     },
     plugins: [
         [
