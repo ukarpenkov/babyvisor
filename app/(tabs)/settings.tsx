a642d80921cbc1c2fa9b6708a6bb03b820b40734
ukarpenkov <truloveu@gmail.com>
2025-06-22 08:10:57 +0300

    simple camera

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,21 @@
-import { View } from 'react-native'
-import CameraComponent from '../../components/OneMonth'
+import { Text } from 'react-native'
+import {
+    Camera,
+    useCameraDevices,
+    useCameraPermission,
+} from 'react-native-vision-camera'
 
-export default function Serrtings() {
+export default function Settings() {
+    const devices = useCameraDevices()
+    const device = devices?.find((d) => d.position === 'back')
+    const { hasPermission } = useCameraPermission()
+
+    if (!hasPermission) return <Text>NOOO</Text>
+    if (device == null) return <Text>NOOO DEVICE</Text>
     return (
-        <View>
-            <CameraComponent />
-        </View>
+        <Camera
+            device={device}
+            isActive={true}
+        />
     )
 }
