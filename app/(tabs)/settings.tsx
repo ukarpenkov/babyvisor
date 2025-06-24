18f4c07154a170237202ecc4a18b75d08630de5e
ukarpenkov <truloveu@gmail.com>
2025-06-24 10:23:16 +0300

    position camera

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {
+    Platform,
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native'
 import {
     Camera,
     useCameraDevice,
@@ -9,16 +16,30 @@ export default function Settings() {
     const device = useCameraDevice('back')
     const { hasPermission } = useCameraPermission()
 
-    if (!hasPermission) return <Text>No</Text>
-    if (device == null) return <Text>No</Text>
+    if (!hasPermission) return <Text>No permission</Text>
+    if (device == null) return <Text>No device</Text>
+
     return (
-        <View>
-            <Text>Camera</Text>
-            <Camera
-                style={StyleSheet.absoluteFill}
-                device={device}
-                isActive={true}
-            />
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+                <Text>Camera</Text>
+                <Camera device={device} isActive={true} style={styles.camera} />
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+    },
+    content: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+        paddingBottom: Platform.OS === 'android' ? 24 : 0,
+    },
+    camera: {
+        flex: 1,
+    },
+})
